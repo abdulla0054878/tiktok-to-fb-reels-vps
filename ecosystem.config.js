baea784a8022cd581844fd0d0805612bffa33041
abdulla0054878 <145589882+abdulla0054878@users.noreply.github.com>
2025-09-25 23:08:00 +0600

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+module.exports = {
+  apps: [
+    {
+      name: "tiktok-fb-bot",
+      script: "./update.sh",
+      interpreter: "bash",
+      cron_restart: `*/${process.env.CRON_INTERVAL_MINUTES || 5} * * * *`,
+      env: {
+        TIKTOK_PROFILE: process.env.TIKTOK_PROFILE,
+        FB_PAGE_LINK: process.env.FB_PAGE_LINK,
+        FB_CAPTION: process.env.FB_CAPTION,
+        FB_COOKIES_PATH: process.env.FB_COOKIES_PATH
+      }
+    }
+  ]
+};
